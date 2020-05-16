90113d242debfcdfab3b9c79513b53d12f07b598
Mitchel Severe <mitchelsevere@gmail.com>
2020-05-16 00:23:36 -0400

    Add OptionsPane Component as child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Header } from 'Components';
+import { Header, OptionsPane } from 'Components';
 function App() {
   return (
     <div>
       <Header />
+      <OptionsPane />
     </div>
   );
 }
