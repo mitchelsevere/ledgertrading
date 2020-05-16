2c51e0c2ac2ea1ee835a1bc91e931be598e09928
Mitchel Severe <mitchelsevere@gmail.com>
2020-05-16 09:52:33 -0400

    Add export for OptionsBoard and Option component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './Header';
 export { default as OptionsHeader } from './OptionsHeader';
 export { default as OptionsPane } from './OptionsPane';
+export { default as OptionsBoard } from './OptionsBoard';
+export { default as Option } from './Option';
