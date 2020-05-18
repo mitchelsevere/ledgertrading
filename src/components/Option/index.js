d2ab9aca9baef1bc45414ea5c8000c3656be423a
Mitchel Severe <mitchelsevere@gmail.com>
2020-05-17 22:18:03 -0400

    Option component with number conversion and useStore helper functions

--- src/components/Option/index.js
+++ src/components/Option/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import { useDispatch, ReactReduxContext } from 'react-redux';
 import styles from './Option.module.css';
 
+import * as actions from 'Actions';
+
 function Option(props) {
+  const numberConversion = (number, bool = true) => {
+    if (bool) {
+      return (number / 100)
+        .toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        })
+        .slice(1);
+    }
+    return (number / 100)
+      .toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      })
+      .slice(1, -3);
+  };
+
+  const useStore = () => {
+    const { store } = useContext(ReactReduxContext);
+    const { getState, dispatch } = store;
+    const state = getState();
+    return [state, dispatch];
+  };
+
+  const [store] = useStore();
+  const dispatch = useDispatch();
+  const date = store.options.currentDate;
+  dispatch(actions.setCurrentDate(props.date));
+
   return (
-    <tr className={styles.option}>
-      <td>{props.coi}</td>
-      <td>{props.cbid}</td>
-      <td>{props.cask}</td>
-      <td>{props.strike}</td>
-      <td>{props.pbid}</td>
-      <td>{props.pask}</td>
-      <td>{props.poi}</td>
-    </tr>
+    <Fragment>
+      {date !== props.date && (
+        <tr className={styles.date}>
+          <td>{props.date}</td>
+        </tr>
+      )}
+      <tr className={styles.option}>
+        <td>{props.callOi || '·'}</td>
+        <td>{numberConversion(props.callBid) || '·'}</td>
+        <td>{numberConversion(props.callAsk) || '·'}</td>
+        <td className={styles.strike}>
+          {numberConversion(props.strike, false) || '·'}
+        </td>
+        <td>{numberConversion(props.putBid) || '·'}</td>
+        <td>{numberConversion(props.putAsk) || '·'}</td>
+        <td>{props.putOi || '·'}</td>
+      </tr>
+    </Fragment>
   );
 }
 
